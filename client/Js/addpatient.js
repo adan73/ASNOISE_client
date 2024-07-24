aa339f45e0c0366a2415c663dd3ad21e0a31e10e
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-25 00:54:07 +0300

    add a new function that take the information we get from the form and add them in the patient db

---
+++ client/Js/addpatient.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault(); // Prevent default form submission
+
+        // Retrieve values from the form fields
+        const first_name = document.getElementById("fname").value;
+        const last_name = document.getElementById("lname").value;
+        const patient_id = document.getElementById("id").value;
+        const hmo = document.getElementById("HMO").value;
+        const adhdStage = document.getElementById("ADHD").value;
+        const age = document.getElementById("Age").value;
+        const phone = document.getElementById("Phone").value;
+        const email = document.getElementById("email").value;
+        const career = document.getElementById("Career").value;
+        const address = document.getElementById("Address").value;
+        const photo = 'https://cdn-icons-png.freepik.com/512/3686/3686930.png';
+
+        
+        if (!first_name || !last_name || !patient_id || !hmo || !adhdStage || !age || !phone || !email || !career || !address) {
+            alert("Please fill out all required fields.");
+            return; 
+        }
+        try {
+          const response = await fetch('https://asnoise-4.onrender.com/api/patients/addPatient', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({first_name,last_name,patient_id,hmo,adhdStage, age, career,address, phone, email, photo})
+          });
+      
+          if (!response.ok) {
+              const errorText = await response.text();
+              throw new Error(`Network response was not ok: ${errorText}`);
+          }
+      
+          const data = await response.json();
+      
+          if (data.success) {
+              window.location.href = "Doctor_homepage.html";
+          } else {
+              alert("Failed to add patient.");
+          }
+      } catch (error) {
+          console.error('Error during adding:', error);
+          alert(`An error occurred while adding the patient: ${error.message}`);
+      }
+      
+    });
+});
